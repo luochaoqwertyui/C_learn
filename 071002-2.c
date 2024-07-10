52ab360ae8eaab5a0e2f90e4c2860828668740e2
luochaoqwertyui <luochaoqwertyui@qq.com>
2024-07-11 01:15:26 +0800

    extern sizeof

---
+++ 071002-2.c
@@ -0,0 +1,2 @@
+#define _CRT_SECURE_NO_WARNINGS
+int d = 10;
--- 071002.c
+++ 071002.c
@@ -1,13 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+extern int d;
+void c()
+{
+	printf("d=%d\n", d);
+}
+
 int main() {
 	printf("hi\n");
+		c();
 	int num1 = 0;
 	int num2 = 0;
 	int age = 10;
 	printf("a=%d\n" ,age);
-	scanf("%d %d\n",&num1, &num2);
+	printf("%d",sizeof())
+	scanf("%d %d",&num1, &num2);
 	int b = num1 + num2;
-	printf("%d",b);
+	printf("%d\n",b);
+	printf("%d\n",d);
 	return 0;
 	}
